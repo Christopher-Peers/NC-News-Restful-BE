9c1aea7cc624454382fa06e38b03aa577ade9be5
Christopher Peers <christopher_peers@outlook.com>
2018-02-24 12:18:06 +0000

    getArticlesByTopic function added

--- controllers/topicControllers.js
+++ controllers/topicControllers.js
@@ -1,18 +1,26 @@
+const Articles = require('../models/articles');
 const Topics = require('../models/topics');
 
-function getAllTopics (req, res, next) {
-    console.log('getAllTopics called')
+function getAllTopics(req, res, next) {
+  console.log('getAllTopics called')
 
-    return Topics.find().lean()
-        .then(allTopics => {
-            res.status(200).json({topics : allTopics});
-        })
-        .catch(next)
+  return Topics.find().lean()
+    .then(allTopics => {
+      res.status(200).json({ topics: allTopics });
+    })
+    .catch(next)
 
 }
 
-function getArticlesByTopic (req, res, next) {
-    console.log('getArticlesByTopic called')
+function getArticlesByTopic(req, res, next) {
+  console.log('getArticlesByTopic called')
+  const topicToSearchFor = req.params.topic;
+
+  return Articles.find({ belongs_to: topicToSearchFor }).lean()
+    .then(articlesByTopic => {
+      res.status(200).json({ articles: articlesByTopic})
+    })
+    .catch(next)
 }
 
 module.exports = { getAllTopics, getArticlesByTopic }
