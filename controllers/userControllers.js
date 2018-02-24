f2f3afdaf3035a474685639af4d9db781302dd34
Christopher Peers <christopher_peers@outlook.com>
2018-02-24 13:00:14 +0000

    getUser function added

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,9 +2,10 @@ const Users = require('../models/users');
 
 function getAllUsers(req, res, next) {
   console.log('getAllusers called')
+
   return Users.find().lean()
     .then(allUsers => {
-      res.status(200).json({ users: allUsers})
+      res.status(200).json({ users: allUsers })
     })
     .catch(next)
 }
@@ -16,6 +17,14 @@ function getUserRepo(req, res, next) {
 
 function getUser(req, res, next) {
   console.log('getUser called')
+
+  const userHandle = req.params.username;
+  return Users.findOne({username: userHandle}).lean()
+    .then(singleUser => {
+      res.status(200).json({users: singleUser})
+    })
+    .catch(next)
+
 }
 
 module.exports = { getAllUsers, getUserRepo, getUser }
