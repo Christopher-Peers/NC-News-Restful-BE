7c0cd8213fe7005adddcf7af0a5ba674d2eee5a2
Christopher Peers <christopher_peers@outlook.com>
2018-02-24 12:30:59 +0000

    changeCommentVote function added

--- controllers/commentControllers.js
+++ controllers/commentControllers.js
@@ -1,9 +1,24 @@
-function changeCommentVote (req, res, next) {
-    console.log('changeCommentVote called')
+const Comments = require('../models/comments');
+
+function changeCommentVote(req, res, next) {
+  console.log('changeCommentVote called')
+
+  const commentId = req.params.comment_id
+  let modifier;
+
+  if (req.query.vote === 'up') modifier = 1;
+  else if (req.query.vote === 'down') modifier = -1;
+  // else // create an error to pass to error handling middleware once written
+
+  return Comments.findByIdAndUpdate(commentId, { $inc: { votes: modifier } }, { new: true })
+    .then(updatedCommentVotes => {
+      res.status(200).json(updatedCommentVotes) // better status code?
+    })
+    .catch(next)
 }
 
-function deleteUserComment (req, res, next) {
-    console.log('deleteUserComment called')
+function deleteUserComment(req, res, next) {
+  console.log('deleteUserComment called')
 }
 
 module.exports = { changeCommentVote, deleteUserComment }
